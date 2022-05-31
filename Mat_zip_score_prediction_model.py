de367a4e4289ddbfe2a4b1d4694507488c74aed6
ASIA-10 <dayoung.biz@gmail.com>
2022-05-31 14:47:04 +0900

    Mat_zip_score_prediction_model02

--- Mat_zip_score_prediction_model.py
+++ Mat_zip_score_prediction_model.py
@@ -4,21 +4,17 @@ from tensorflow.keras.models import *
 from tensorflow.keras.layers import *
 
 X_train, X_test, Y_train, Y_test = np.load(
-    './Crawling_data/mat_zip_data_max189_wordsize_25006.npy',
+    './Crawling_data/mat_zip_data_max189_wordsize_24046.npy',
     allow_pickle=True)
 
 print(X_train.shape, Y_train.shape)
 print(X_test.shape, Y_test.shape)
 
 model = Sequential()
-model.add(Embedding(25006, 300, input_length= 189))
-
+model.add(Embedding(24046, 300, input_length= 189))
 model.add(Conv1D(32, kernel_size= 5, padding='same', activation= 'relu'))
-
 model.add(MaxPool1D(pool_size=1))
-
 model.add(LSTM(128, activation='tanh', return_sequences=True))
-
 model.add(Dropout(0.3))
 model.add(LSTM(64, activation='tanh', return_sequences=True))
 model.add(Dropout(0.3))
@@ -26,14 +22,14 @@ model.add(LSTM(64, activation='tanh'))
 model.add(Dropout(0.3))
 model.add(Flatten())
 model.add(Dense(128, activation= 'relu'))
-model.add(Dense(5, activation='softmax'))
+model.add(Dense(4, activation='softmax'))
 
 model.summary()
 
 model.compile(loss= 'categorical_crossentropy',
               optimizer='adam', metrics=['accuracy'])
 fit_hist = model.fit(X_train, Y_train, batch_size=128,
-                     epochs=3, validation_data=(X_test, Y_test))
+                     epochs=50, validation_data=(X_test, Y_test))
 model.save('./models/mat_zip_score_prediction_model{}.h5'.format(
     fit_hist.history['val_accuracy'][-1]))
 plt.plot(fit_hist.history['val_accuracy'], label= 'val_accuracy')
