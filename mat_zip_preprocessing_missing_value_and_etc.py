5a08aa7f80218cc578ead3482877577794b4130c
이준한 <54476630+LEEJUNHAN9305@users.noreply.github.com>
2022-05-31 14:41:29 +0900

    e_and_etc.py mat_zip_preprocessing_missing_value_and_etc_4th

--- mat_zip_preprocessing_missing_value_and_etc.py
+++ mat_zip_preprocessing_missing_value_and_etc.py
@@ -4,28 +4,31 @@ from sklearn import preprocessing
 import pandas as pd
 pd.set_option('display.max_seq_items', None)
 df = pd.read_csv('./Crawling_data/crawling_data_concat_final.csv')
-
+#리뷰평이 10자 미만인 리뷰 NaN화
+for i in range(len(df)):
+    if len(df.iloc[i, 1]) < 10 :
+        df.iloc[i, 1] = np.nan
+#리뷰평이 공백인 리뷰 NaN화
 for i in range(len(df)):
     if df.iloc[i, 1] == ' ':
         df.iloc[i, 1] = np.nan
-print(df.isna().sum())
+#리뷰평에서 발견된 광고 NaN화
 for i in range(len(df)):
     if df.iloc[i, 1] == '의정부 배달 불가능한 음식점 배달해드립니다 주문만 해주세요 주문은 카톡 트루미스 친추':
         df.iloc[i, 1] = np.nan
-print(df.isna().sum())
+#NaN값 제거
 df.dropna(inplace=True)
+#1점 데이터 제거 - 데이터가 적음
 df1 = df[df['star_score'] == 1].index
 df = df.drop(df1)
-
-#downsampling
-df_5 = df[df['star_score'] == 5]
-df_4 = df[df['star_score'] == 4]
-df_3 = df[df['star_score'] == 3]
-df_2 = df[df['star_score'] == 2]
-df_5 = df_5.sample(8500)
-df_4 = df_4.sample(8500)
-df_3 = df_3.sample(8500)
-df = pd.concat([df_5, df_4, df_3, df_2], axis=0)
+#downsampling -> 안 하기로 결정
+# df_5 = df[df['star_score'] == 5]
+# df_4 = df[df['star_score'] == 4]
+# df_3 = df[df['star_score'] == 3]
+# df_2 = df[df['star_score'] == 2]
+# df_5 = df_5.sample(8500)
+# df_4 = df_4.sample(8500)
+# df_3 = df_3.sample(8500)
+# df = pd.concat([df_5, df_4, df_3, df_2], axis=0)
 df.reset_index(inplace=True, drop=True)
-# print(df['star_score'].value_counts(ascending=True))
-df.to_csv('./Crawling_data/mat_zip_preprocessing_03.csv', index = False)
+df.to_csv('./Crawling_data/mat_zip_preprocessing_05.csv', index = False)
